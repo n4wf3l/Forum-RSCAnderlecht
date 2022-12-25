d6ababe2cc30b48acd59f4868a42be36fac835a6
n4wf3l <nawfel.ajari@student.ehb.be>
2022-12-25 23:47:59 +0100

    Github

---
+++ SQLQuery3.sql
@@ -0,0 +1,11 @@
+﻿USE [aspnet-RSCAnderlechtF-3E7AE629-3255-424A-9837-121B340A5182]
+GO
+
+ 
+
+INSERT INTO [dbo].[AspNetUserRoles]
+([UserId]
+,[RoleId])
+VALUES
+('880e35be-5cbd-4b88-a64a-bb0175133b2d', '68bdf9cb-4866-444d-8cf5-56d54170dc81')
+GO
